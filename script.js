00250e2c7477a14741cdd5de72369836dc6c1916
JaynesN <jaynesn763@my.clevelandcc.edu>
2024-12-13 09:30:16 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function linkShow() {
+        document.getElementById("linksshow").innerHTML = "Links ^";
+        document.getElementById("links").innerHTML = "<a href=https://www.youtube.com/@JustRollWithIt/> Youtube </a> <br> <a href=https://www.patreon.com/JustRollWithIt> Patreon </a>";
+    }
